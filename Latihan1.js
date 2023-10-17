67cce2f63ea8cc4ece331cbf3f8f292d71974a13
irawandwi0803 <dwiirawan0803@gmail.com>
2023-10-17 10:53:57 +0700

    commit pertama

---
+++ Latihan1.js
@@ -0,0 +1,8 @@
+//membuat variable
+    var nama = "Irawan Dwi Cahyo";
+    var age = 23;
+    var kota = "Tangerang";
+//menampilkan output pada console
+    console.log("nama: ",nama);
+    console.log("umur: ",age);
+    console.log("Tempat tinggal: ",kota)
